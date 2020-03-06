42ba701e66597c01f778cadafe8cb8c6e6656d83
aman345 <13480902+aman345@users.noreply.github.com>
2020-03-06 09:53:38 +0530

    Add files via upload

--- BirthdayNotifier.py
+++ BirthdayNotifier.py
@@ -1,19 +1,22 @@
 import notify2
 from datetime import date
+import logging
+
+#capturing current date and formating it 
 dt = date.today()
 t = dt.strftime("%d/%b")
 
-notify2.init("Demo application")
+notify2.init("Birthday Reminder  application")
 
+#file source
 f = open("Birthday.txt")
 f1 = f.readlines()
+
 try:
     for i in f1:
+        print("here in loop")
         n = i.split(":")
-        if (n[1] == t):
-            n = notify2.Notification("Birthday", "Happy birthday ,{}".format(n[0]))
-            n.show()
-        else:
-            notify2.Notification("No Birthday today").show()
-except expression as identifier:
-    print(expression)
+        if (n[1].strip("\n") == t):
+            notify2.Notification("Birthday", "Happy birthday ,{}".format(n[0])).show() 
+except:
+    print("Error occured while reading file....")
