4f57fcb58fdd7f463d78c894fb0da4dedd3a2898
aman345 <13480902+aman345@users.noreply.github.com>
2021-10-16 20:26:23 +0530

    Update BirthdayNotifier.py

--- BirthdayNotifier.py
+++ BirthdayNotifier.py
@@ -1,6 +1,6 @@
 import notify2
 from datetime import date
-import logging
+
 
 #capturing current date and formating it 
 dt = date.today()
@@ -14,7 +14,6 @@ f1 = f.readlines()
 
 try:
     for i in f1:
-        print("here in loop")
         n = i.split(":")
         if (n[1].strip("\n") == t):
             notify2.Notification("Birthday", "Happy birthday ,{}".format(n[0])).show() 
